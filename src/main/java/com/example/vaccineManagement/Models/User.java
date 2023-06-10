6c2ee11d5b7b53ca68c19c4bcdad5150bb6f9799
Neelam Dubey <ashidubey8208@gmail.com>
2023-06-10 12:13:27 +0530

    first commit

---
+++ src/main/java/com/example/vaccineManagement/Models/User.java
@@ -0,0 +1,84 @@
+package com.example.vaccineManagement.Models;
+import com.example.vaccineManagement.Enums.Gender;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Column(name = "user_name")
+    private String name;
+
+    private int age;
+
+    @Column(unique = true)
+    private String emailId;
+
+    //@Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private String mobileNo;
+
+    @OneToOne(mappedBy = "user",cascade = CascadeType.ALL)
+    private Dose1 dose;
+
+    public Dose1 getDose() {
+        return dose;
+    }
+
+    public void setDose(Dose1 dose) {
+        this.dose = dose;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+}
